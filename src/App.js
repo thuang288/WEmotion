e3b3c71c30b2ae8fc20684eeafddcb6b2fd8a4f2
Tommy Huang <48419590+thuang288@users.noreply.github.com>
2021-05-25 14:25:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React from "react";
 import "./App.css";
-// import {
-//   Home,
-//   Login,
-//   Signup,
-//   New,
-//   Navbar,
-// } from "./components/index";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
@@ -21,8 +14,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import PrivateRoute from "./components/PrivateRoute";
 import SampleFirebaseComponent from "./components/SampleFirebaseComponent";
 
-// import SampleFirebaseComponent from "./components/SampleFirebaseComponent";
-
 const App = () => {
   return (
     <div>
@@ -31,7 +22,7 @@ const App = () => {
           <Navbar />
           <Switch>
             {/* // try using components  */}
-            {/* change Route to PrivateRoute for /New and /Date when not testing */}
+            {/* change Route to PrivateRoute for and /Date when not testing */}
             <Route exact path="/New" component={New} />
             <Route path="/Signup" component={Signup} />
             <Route path="/Login" component={Login} />
